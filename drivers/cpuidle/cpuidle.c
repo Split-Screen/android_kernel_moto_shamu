a47328ac1a794bc393d8d68573249f92ece3380f
Viresh Kumar <viresh.kumar@linaro.org>
2013-10-03 21:26:48 +0530

    cpuidle: reduce code duplication inside cpuidle_idle_call()
    We are doing this twice in cpuidle_idle_call() routine:
    	drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP
    
    Would be better if we actually store this in a local variable and
    use that. That reduces code duplication and likely makes this piece
    of code run faster (in case the compiler wasn't able to optimize it
    earlier)
    
    [rjw: Cast the result of bitwise AND to bool explicitly using !!]
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -130,6 +130,7 @@ int cpuidle_idle_call(void)
 	struct cpuidle_device *dev = __this_cpu_read(cpuidle_devices);
 	struct cpuidle_driver *drv;
 	int next_state, entered_state = 0;
+	bool broadcast;
 
 	if (off || !initialized)
 		return -ENODEV;
@@ -160,7 +161,9 @@ int cpuidle_idle_call(void)
 		goto exit;
 	}
 
-	if (drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP)
+	broadcast = !!(drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP);
+
+	if (broadcast)
 		clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER,
 				   &dev->cpu);
 
@@ -170,7 +173,7 @@ int cpuidle_idle_call(void)
 	else
 		entered_state = cpuidle_enter_state(dev, drv, next_state);
 
-	if (drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP)
+	if (broadcast)
 		clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT,
 				   &dev->cpu);
 
