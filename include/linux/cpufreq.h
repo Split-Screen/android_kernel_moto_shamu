d748080f30ec5c6c9ca71c15250258292a39cf91
CPA-Poke <brock.shahan@cox.net>
2014-12-11 20:31:10 -0500

    Add Intelliactive CPU gov
    Change-Id: Ic8c028c7b3116895604ed7e67e0c156ebfa6c9ad

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -421,6 +421,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
 #endif
 
 /*********************************************************************
