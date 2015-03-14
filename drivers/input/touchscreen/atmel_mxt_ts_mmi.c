0d167dc94cccf4c4d2556022152b9a03a30094a8
Björn Hutmacher <bjorn.hutmacher@gmail.com>
2015-03-14 23:27:16 +0100

    Revert "Wake gestures: suspended type changed to atomic_t"
    This reverts commit 0a7417379af51f39a80596e217946b33c13797d4.

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -1418,7 +1418,7 @@ static void mxt_proc_t100_message(struct mxt_data *data, u8 *message)
 			tool = MT_TOOL_FINGER;
 
 #ifdef CONFIG_WAKE_GESTURES
-		if (atomic_read(&data->suspended)) {
+		if (data->suspended) {
 		        x += 5000;
 		}
 #endif
@@ -2520,7 +2520,7 @@ struct mxt_data *gl_mxt_data;
 bool scr_suspended(void)
 {
 	struct mxt_data *mxt_data = gl_mxt_data;
-	return atomic_read(&mxt_data->suspended);
+	return mxt_data->suspended;
 }
 
 void set_internal_dt(bool input)
