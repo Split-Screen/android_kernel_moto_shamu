05119ff0a335eada511ac5ab6d17dcfe2fd3b485
hellsgod <hellsgod@gmx.ch>
2015-06-17 19:14:26 +0200

    msm_adreno_tz: Remove power suspend hooks

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -21,7 +21,6 @@
 #include <linux/msm_adreno_devfreq.h>
 #include <soc/qcom/scm.h>
 #include "governor.h"
-#include <linux/powersuspend.h>
 
 
 static DEFINE_SPINLOCK(tz_lock);
@@ -138,7 +137,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
 	 */
-	if (suspended || power_suspended) {
+	if (suspended) {
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
